54c88e7e8ad74fa9c2913adb76fb5f4d3c3ebe5d
andrew.zubko <andrewzubko@a.local>
2017-09-05 17:43:15 +0300

    initial import

---
+++ contracts/RyftsICO.sol
@@ -0,0 +1,157 @@
+pragma solidity ^0.4.13;
+
+
+import '../libs/contracts/LoggedERC20.sol';
+
+contract RyftsICO is LoggedERC20 {
+
+    uint256 public icoSince;
+    uint256 public icoTill;
+
+    uint256 public minCap;
+
+    uint256 public tokenPrice;
+
+    uint256 public collectedEthers;
+
+    bool public isIcoFinished;
+    bool public isRefundAllowed;
+
+    mapping (address => uint256) sentEthers;
+
+    function RyftsICO(
+        uint256 _tokenPrice,
+        uint256 _icoSince,
+        uint256 _icoTill,
+        uint256 _minCap,
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol,
+        bool _locked
+    ) LoggedERC20(initialSupply, tokenName, 8, tokenSymbol, false, _locked) {
+        standard = 'Ryfts 0.1';
+        icoSince = _icoSince;
+        icoTill = _icoTill;
+        minCap = _minCap;
+        tokenPrice = _tokenPrice;
+    }
+
+    function getBonusAmount(uint256 time, uint256 amount) returns (uint256) {
+        if(time < icoSince) {
+            return 0;
+        }
+
+        if(time - icoSince <= 10800) {             // 3h since ico => reward 25%
+            return amount * 25 / 100;
+        }
+        else if(time - icoSince <= 21600) {        // 6h since ico => reward 15%
+            return amount * 15 / 100;
+        }
+        else if(time - icoSince <= 32400) {        // 9h since ico => reward 5%
+            return amount * 5 / 100;
+        }
+
+        return 0;
+    }
+
+    function buy(address _address, uint256 time, uint256 value) internal returns (bool) {
+        if (locked == true) {
+            return false;
+        }
+
+        if (time < icoSince) {
+            return false;
+        }
+
+        if (time > icoTill) {
+            return false;
+        }
+
+        if (value == 0) {
+            return false;
+        }
+
+        uint256 amount = value * (uint256(10) ** decimals) / tokenPrice;
+
+        if (amount == 0) {
+            return false;
+        }
+
+        uint256 totalAmount = amount + getBonusAmount(now, amount);
+
+        uint256 selfBalance = valueAt(loggedBalances[this], block.number);
+        uint256 holderBalance = valueAt(loggedBalances[_address], block.number);
+
+        if (selfBalance < totalAmount) {
+            return false;
+        }
+
+        if (holderBalance + totalAmount < holderBalance) {
+            return false;
+        }
+
+        setBalance(_address, holderBalance + totalAmount);
+        setBalance(this, selfBalance - totalAmount);
+
+        collectedEthers += value;
+
+        sentEthers[_address] += value;
+
+        Transfer(this, _address, totalAmount);
+
+        return true;
+    }
+
+    function() payable {
+        bool status = buy(msg.sender, now, msg.value);
+        require(status == true);
+    }
+
+    function setTokenPrice(uint256 _value) onlyOwner {
+        tokenPrice = _value;
+    }
+
+    function setICOPeriod(uint256 _icoSince, uint256 _icoTill) onlyOwner {
+        icoSince = _icoSince;
+        icoTill = _icoTill;
+    }
+
+    function setLocked(bool _locked) onlyOwner {
+        locked = _locked;
+    }
+
+    function icoFinished() {
+        if(now > icoTill) {
+            if(collectedEthers >= minCap) {
+                isRefundAllowed = false;
+            }
+            else {
+                isRefundAllowed = true;
+            }
+
+            isIcoFinished = true;
+        }
+    }
+
+    function refund() returns (bool) {
+        if(isIcoFinished == true && isRefundAllowed == true) {
+            if(sentEthers[msg.sender] > 0) {
+                uint256 ethersToSent = sentEthers[msg.sender];
+
+                sentEthers[msg.sender] = 0;
+
+                msg.sender.transfer(ethersToSent);
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    function transferEthers() onlyOwner {
+        if(isIcoFinished == true && isRefundAllowed == false) {
+            owner.transfer(this.balance);
+        }
+    }
+}
