884c647f045ab14f5a67e3850372928112243bd1
morrigan <belma.dzi@gmail.com>
2018-05-11 15:07:09 +0200

    Update to ganache

--- truffle.js
+++ truffle.js
@@ -3,7 +3,9 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      gasPrice: 1,
+      gas: 4700000,
     }
   }
 };
